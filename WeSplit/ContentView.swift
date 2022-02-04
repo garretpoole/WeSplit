bdba00e5345034f93ab17b34303b0819b01a0a15
Garret Poole <garretpoole@users.noreply.github.com>
2022-02-04 13:41:18 -0800

    modifying program state

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,16 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    //allows data to be stored seperate from the View struct
+    //for simple properties stored locally in one view
+    @State private var tapCount = 0
     var body: some View {
-        NavigationView{
-            Form{
-                Section{
-                    Text("Hello, world!")
-                }
-            }
-            //belongs to form
-            .navigationTitle("SwiftUI")
-            .navigationBarTitleDisplayMode(.inline)
+        Button("Tap Count: \(tapCount)"){
+            tapCount += 1
         }
     }
 }
