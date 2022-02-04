049e719487df56399dd5318c6070555d7336b60d
Garret Poole <garretpoole@users.noreply.github.com>
2022-02-04 14:55:39 -0800

    added "done" to iPhone decimal keyboard

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     //numPeople sets index row in the picker (2 -> 4 people)
     @State private var numPeople = 2
     @State private var tipPercent = 20
-    
+    @FocusState private var amountFocused: Bool
     let tipPercentages = [0, 5, 10, 15, 20, 25]
     
     //computed property
@@ -35,6 +35,7 @@ struct ContentView: View {
                         //Locale is struct that gets area of user from iOS settings
                         .currency(code: Locale.current.currencyCode ?? "USD"))
                         .keyboardType(.decimalPad)
+                        .focused($amountFocused)
                     Picker("Number of People", selection: $numPeople){
                         ForEach(2..<100){
                             Text("\($0) people")
@@ -53,10 +54,18 @@ struct ContentView: View {
                 }
                 Section{
                     Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "USD"))
-                } header: {
-                    Text("Total Per Person")
                 }
                 .navigationTitle("WeSplit")
+                //lets specify toolbar items for a group
+                .toolbar {
+                    //blace buttons for toolbar
+                    ToolbarItemGroup(placement: .keyboard){
+                        Spacer()
+                        Button("Done") {
+                            amountFocused = false
+                        }
+                    }
+                }
             }
         }
     }
