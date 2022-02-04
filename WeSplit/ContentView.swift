d1a33abd444e8cf8aff99818dbfa057ee14705b7
Garret Poole <garretpoole@users.noreply.github.com>
2022-02-04 14:18:44 -0800

    created picker for num of people, added title

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,21 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var checkAmount = 0.0
+    //numPeople sets index row in the picker
     @State private var numPeople = 2
     @State private var tipPercent = 20
     
     let tipPercentages = [0, 5, 10, 15, 20, 25]
     
     var body: some View {
-        Form{
-            Section{
-                TextField("Amount", value: $checkAmount, format:
-                    //Locale is struct that gets area of user from iOS settings
-                    .currency(code: Locale.current.currencyCode ?? "USD"))
-                    .keyboardType(.decimalPad)
-            }
-            Section{
-                Text(checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+        NavigationView{
+            Form{
+                Section{
+                    TextField("Amount", value: $checkAmount, format:
+                        //Locale is struct that gets area of user from iOS settings
+                        .currency(code: Locale.current.currencyCode ?? "USD"))
+                        .keyboardType(.decimalPad)
+                    Picker("Number of People", selection: $numPeople){
+                        ForEach(2..<100){
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                Section{
+                    Text(checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                }
+                .navigationTitle("WeSplit")
             }
         }
     }
